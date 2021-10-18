d163ad4cf2d7a66b10844ae68a4fde547d32f6c4
Vitor Melo <vitor.vmelo@tdx.com.br>
2021-10-18 18:16:57 -0300

    Ajuste no exemplo

--- example/via_cep_example.dart
+++ example/via_cep_example.dart
@@ -1,25 +1,20 @@
-
-
 import 'package:flutter_cep2/flutter_cep2.dart';
 
 void main() async {
   var CEP = flutter_cep2();
 
-  var result = await CEP.searchCEP('12512170', 'json', '');
-
-  // Sucesso
-  if (result.getResponse() == 200) {
-    print('CEP: '+result.getCEP());
-    print('Logradouro: '+result.getLogradouro());
-    print('Complemento: '+result.getComplemento());
-    print('Bairro: '+result.getBairro());
-    print('Localidade: '+result.getLocalidade());
-    print('UF: '+result.getUF());
-    print('Unidade: '+result.getUnidade());
-    print('IBGE '+result.getIBGE());
-    print('GIA: '+result.getGIA());
-  } else {
-    print('Código de Retorno: '+result.getResponse().toString());
-    print('Erro: '+result.getBody());
+  try {
+    var result = await CEP.search('12512170');
+    print('CEP: ' + result.cep);
+    print('Logradouro: ' + result.logradouro);
+    print('Complemento: ' + result.complemento);
+    print('Bairro: ' + result.bairro);
+    print('Localidade: ' + result.localidade);
+    print('UF: ' + result.uf);
+    print('Unidade: ' + result.unidade);
+    print('IBGE ' + result.ibge);
+    print('GIA: ' + result.gia);
+  } catch (e) {
+    print(e);
   }
 }
